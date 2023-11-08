b98549f3729e1393156e3424b163979d496806f0
lihi33 <141512720+lihi33@users.noreply.github.com>
2023-11-08 20:24:40 +0200

    adding new file

---
+++ src/test/java/tests/loginTests/positiveTests/PositiveTests.java
@@ -0,0 +1,13 @@
+package tests.loginTests.positiveTests;
+
+import org.testng.annotations.Test;
+
+public class PositiveTests {
+
+
+    @Test
+    public void hello (){
+        System.out.println("hello");
+
+    }
+}
